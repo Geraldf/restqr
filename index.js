6f1f0c9bf7145dd2fce7d21c439209b1a9ebef1f
DESKTOP-Q0M7TB8\Gerald Fuchs <gerald@fuchsclan.de>
2018-06-12 11:36:26 +0200

    initial

---
+++ index.js
@@ -0,0 +1,42 @@
+const { promisify } = require('util')
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
+
+// QRCode generation requirements
+const QRCode = require('qrcode')
+const genDataUrl = promisify(QRCode.toDataURL.bind(QRCode))
+
+// Express requirements
+const bodyParser = require('body-parser')
+const express = require('express')
+
+// Configure Express app
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+/**
+ * Receives a POST request with a body that contains a 'payload' attribute
+ * and returns the base64 data-url.
+ * @returns {{ src: String }}
+ */
+app.get('/', async (req, res) => {
+    payload = req.query.payload
+
+    if (payload === '') {
+        return res
+            .status(422)
+            .json({ err: 'Missing required parameter: "payload"' })
+    }
+
+    const dataUrl = await genDataUrl(payload)
+
+    return res.json({ src: dataUrl })
+})
+
+app.listen(
+    PORT,
+    () => {
+        console.log('Server is running')
+    }
+)
